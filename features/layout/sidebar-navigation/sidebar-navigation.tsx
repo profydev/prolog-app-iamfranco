6de6999fa76744a8a7c8283efa5c8e2daf6f0003
Franco Chan <kinfaifrancochan@gmail.com>
2023-10-30 20:08:09 +0000

    Fix navigation support button to open email app (#4)
    with cypress test to check component has the correct href

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -79,11 +79,12 @@ export function SidebarNavigation() {
             ))}
           </ul>
           <ul className={styles.list}>
-            <MenuItemButton
+            <MenuItemLink
               text="Support"
               iconSrc="/icons/support.svg"
+              href="mailto:support@prolog-app.com?subject=Support Request: "
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              isActive={false}
             />
             <MenuItemButton
               text="Collapse"
